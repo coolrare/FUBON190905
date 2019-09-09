ab625c516a257910b4604d7cee4c8efab5858b01
Will <doggy.huang@gmail.com>
2019-09-09 16:25:01 +0800

    示範可帶參數的自訂驗證器

--- src/app/shared/forbiddenWill.ts
+++ src/app/shared/forbiddenWill.ts
@@ -1,10 +1,12 @@
 import { AbstractControl } from '@angular/forms';
-export function forbiddenWill(control: AbstractControl) {
-  if (control.value) {
-    const str = control.value as string;
-    if (str.indexOf('Will') >= 0) {
-      return { forbiddenWill: true };
+export function forbiddenTerm(term: string) {
+  return (control: AbstractControl) => {
+    if (control.value) {
+      const str = control.value as string;
+      if (str.indexOf(term) >= 0) {
+        return { forbiddenWill: true };
+      }
     }
-  }
-  return null;
+    return null;
+  };
 }
